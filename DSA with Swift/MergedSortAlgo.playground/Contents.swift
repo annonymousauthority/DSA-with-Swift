642e7aad9ca5a4b0d95283140dca1de463bdd039
Augustine Francis <enyiaugustinefrancis@gmail.com>
2022-09-05 23:47:09 +0100

    Update Contents.swift

--- DSA with Swift/MergedSortAlgo.playground/Contents.swift
+++ DSA with Swift/MergedSortAlgo.playground/Contents.swift
@@ -29,3 +29,28 @@ func mergedSortAlgo<T: Comparable>(_ array: [T]) -> [T] {
 }
 mergedSortAlgo(["Aaron Allum", "Aaron Augustine", "Andrew Tate", "Barnabas Philip", "Felix Camile"])
 
+public func findDayoftheWeek(_ S : inout String, _ K : Int) -> String {
+        // write your code in Swift 4.2.1 (Linux)
+    let daysIndex = K % 6
+    print(daysIndex)
+    let day_of_the_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    let totalIndex = day_of_the_week.count - 1
+    for (index,day) in day_of_the_week.enumerated(){
+        if (S == day){
+            if daysIndex < 3 && daysIndex + index < 6 {
+                let actualIndex = daysIndex + index
+                return day_of_the_week[actualIndex]
+            }else{
+                let actualIndex = totalIndex - (daysIndex + index)
+                print(actualIndex)
+                return day_of_the_week[abs(actualIndex)]
+            }
+        }
+    }
+
+    return "Not Found"
+
+
+}
+var test = "Wed"
+findDayoftheWeek(&test, 24)
