9e6641cfabfeb44793f5a8daec4113f6df11fd94
Augustine Francis <enyiaugustinefrancis@gmail.com>
2022-09-05 22:20:57 +0100

    Initial Commit

---
+++ DSA with Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DSA with Swift
+//
+//  Created by Augustine Francis on 05/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DSA with Swift/DSA_with_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSA_with_SwiftApp.swift
+//  DSA with Swift
+//
+//  Created by Augustine Francis on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DSA_with_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
